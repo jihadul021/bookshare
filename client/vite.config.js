f98ab6dc548efc81a5f67c13c61eaffb044bc356
jihadul021 <jihadul021@gmail.com>
2026-04-28 22:35:42 +0600

    url changes
    Signed-off-by: jihadul021 <jihadul021@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.NODE_ENV === 'production' ? 'https://bookshare-xbj2.onrender.com' : 'http://localhost:3000', 
+        // target: 'http://localhost:3000', 
+        target: 'https://bookshare-xbj2.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path
       },
       '/uploads': {
-        target: process.env.NODE_ENV === 'production' ? 'https://bookshare-xbj2.onrender.com' : 'http://localhost:3000',
+        // target: 'http://localhost:3000', 
+        target: 'https://bookshare-xbj2.onrender.com',
         changeOrigin: true
       }
     }
